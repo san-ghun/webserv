3c5daa2e8c25d35f90413e2da33c2fbf755e3d2c
Sanghun <po4865@gmail.com>
2024-07-08 23:51:34 +0200

    feat: Add initial class structure for Config
    - Created the class definition in the header file.
    - Defined attributes to store the class data.
    - Included methods for class functionality.

--- include/Config.hpp
+++ include/Config.hpp
@@ -6,13 +6,31 @@
 /*   By: sanghupa <sanghupa@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/30 16:23:00 by sanghupa          #+#    #+#             */
-/*   Updated: 2024/06/30 16:24:32 by sanghupa         ###   ########.fr       */
+/*   Updated: 2024/07/08 23:35:53 by sanghupa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CONFIG_HPP
 # define CONFIG_HPP
 
-# include "webserv.hpp"
+# include <string>
+# include <map>
+
+class	Config
+{
+	public:
+		Config();
+		~Config();
+
+		static void			load(const std::string filename);
+		static std::string	get(const std::string key);
+		static int			getInt(const std::string key);
+		static int			getPort();
+
+	private:
+		static std::map<std::string, std::string>	_configMap;
+		
+		static void			_parseConfigFile(const std::string filename);
+};
 
 #endif
