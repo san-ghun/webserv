a1394d6aedb449c32aee12bc8e3ef1cc8173225c
Sanghun <po4865@gmail.com>
2024-07-30 17:57:45 +0200

    feat: Update Config class to have multiple server with different host
    - Add new structs, ServerConfig and LocationConfig, to store config data.
       - to make instance from class
       - to read during request handling
    - Move and Remove properties that not needed in Config to ServerConfig struct.
    
    TODO: Have to implement getter methods for Config to use it when request handling.

--- include/Config.hpp
+++ include/Config.hpp
@@ -6,7 +6,7 @@
 /*   By: sanghupa <sanghupa@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/30 16:23:00 by sanghupa          #+#    #+#             */
-/*   Updated: 2024/07/22 21:45:18 by sanghupa         ###   ########.fr       */
+/*   Updated: 2024/07/30 17:52:14 by sanghupa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,37 @@
 # include "Location.hpp"
 
 class Location;
+
+struct LocationConfig 
+{
+	std::string path;
+    std::string root;
+    std::vector<std::string> allowed_methods;
+	std::string upload_dir;
+    bool lsdir;
+    std::string cgi_ext;
+    std::string redirection;
+    std::string default_file;
+};
+
+struct ServerConfig 
+{
+    std::string server_name;
+    std::string listen;
+	std::string host;
+	int port;
+    size_t max_body_size;
+    std::string root;
+    std::string default_file;
+    std::string upload_dir;
+	std::string cgi_dir;
+    std::map<int, std::string> error_pages;
+    std::map<std::string, LocationConfig*> map_locations;
+	std::map<std::string, Location*> map_locationObjs;
+};
+
+LocationConfig*	new_locationConfig();
+ServerConfig*	new_serverConfig();
 
 class	Config
 {
@@ -35,17 +66,30 @@ class	Config
 		std::string		get(const std::string key) const;
 		int				getInt(const std::string key) const;
 		bool			getBool(const std::string key) const;
-		std::string		getServerHost() const;
-		int				getPort() const;
-		std::string		getErrorPage(const int code) const;
-		Location*		getLocation(const std::string key) const;
 
+		// TODO: Implement
+		ServerConfig*	getServerByName(const std::string serverName) const;
+		ServerConfig*	getServerByListen(const std::string serverListen) const;
+		ServerConfig*	getServerByHost(const std::string serverHost) const;
+		ServerConfig*	getServerByPort(const int serverPort) const;
+
+		// to server class ...?
+		// std::string		getServerHost() const;
+		// int				getPort() const;
+		// std::string		getErrorPage(const int code) const;
+		// Location*		getLocation(const std::string key) const;
+
+		std::vector<ServerConfig*>			getServers() const;
 		std::map<std::string, std::string>	getMimeTypeMap() const;
-		std::map<int, std::string>			getErrorPageMap() const;
-		std::map<std::string, std::string>	getServerSettingMap() const;
-		std::map<std::string, Location*>	getLocationMap() const;
 		std::map<std::string, std::string>	getConfigMap() const;
 
+		// to server class ...?
+		// std::map<int, std::string>			getErrorPageMap() const;
+		// std::map<std::string, std::string>	getServerSettingMap() const;
+		// std::map<std::string, Location*>	getLocationMap() const;
+
+		// TODO: Implement
+		void			setServer(ServerConfig* server, std::string line);
 		void			setLocation(Location* location, std::string line);
 
 	private:
@@ -53,19 +97,20 @@ class	Config
 		Config();
 		~Config();
 
-		void			_parseKeyValuePair(const std::string line);
-		void			_parseBlock(std::istream& stream, const std::string& blockName);
 		void			_parseConfigFile(const std::string& filename);
-		void			_setHostPort();
+		// void			_setHostPort();
 
+		std::vector<ServerConfig*>			_servers;
 		std::map<std::string, std::string>	_mimeTypeMap;
-		std::map<int, std::string>			_errorPageMap;
-		std::map<std::string, std::string>	_serverSettingMap;
-		std::map<std::string, Location*>	_locationMap;
 		std::map<std::string, std::string>	_configMap;
 
-		int				_port;
-		std::string		_host;
+		// to server class ...?
+		// std::map<int, std::string>			_errorPageMap;
+		// std::map<std::string, std::string>	_serverSettingMap;
+		// std::map<std::string, Location*>	_locationMap;
+
+		// int				_port;
+		// std::string		_host;
 };
 
 #endif
--- src/util/Config.cpp
+++ src/util/Config.cpp
@@ -6,7 +6,7 @@
 /*   By: sanghupa <sanghupa@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/30 16:23:00 by sanghupa          #+#    #+#             */
-/*   Updated: 2024/07/23 20:32:58 by sanghupa         ###   ########.fr       */
+/*   Updated: 2024/07/27 23:16:19 by sanghupa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,21 +17,67 @@
 #include <stdexcept>
 #include <algorithm>
 
+LocationConfig*	new_locationConfig()
+{
+	LocationConfig*	_this = new LocationConfig();
+
+	_this->path = "";
+	_this->root = "";
+	_this->allowed_methods = std::vector<std::string>();
+	_this->upload_dir = "";
+	_this->lsdir = false;
+	_this->cgi_ext = ".py";
+	_this->redirection = "";
+	_this->default_file = "index.html";
+
+	return _this;
+}
+
+ServerConfig*	new_serverConfig()
+{
+	ServerConfig*	_this = new ServerConfig();
+
+	_this->server_name = "";
+	_this->listen = "localhost:80";
+	_this->host = "localhost";
+	_this->port = 80;
+	_this->max_body_size = 1024;
+	_this->root = "/www";
+	_this->default_file = "index.html";
+	_this->upload_dir = "/www/data/uploads";
+	_this->cgi_dir = "/www/cgi-bin";
+	_this->error_pages = std::map<int, std::string>();
+	_this->map_locations = std::map<std::string, LocationConfig*>();
+	_this->map_locationObjs = std::map<std::string, Location*>();
+	
+	return _this;
+}
+
 Config::Config()
 {
 	// TODO: Set default value at here if necessary
-	_port = 80;
-	_host = "0.0.0.0";
 }
 
 Config::~Config()
 {
 	// TODO: Destruct at here if necessary
-	for (std::map<std::string, Location*>::iterator it = _locationMap.begin(); it != _locationMap.end(); it++)
+	for (size_t i = 0; i < _servers.size(); i++)
 	{
-		std::cout << "Delete Location: " << it->first << std::endl;
-		delete it->second;
-		
+		ServerConfig* its = _servers[i];
+		std::map<std::string, LocationConfig*> lMap = its->map_locations;
+		for (std::map<std::string, LocationConfig*>::iterator itl = lMap.begin(); itl != lMap.end(); itl++)
+		{
+			std::cout << "Delete LocationConfig: " << itl->first << std::endl;
+			delete itl->second;
+		}
+		std::map<std::string, Location*> lobjMap = its->map_locationObjs;
+		for (std::map<std::string, Location*>::iterator itl = lobjMap.begin(); itl != lobjMap.end(); itl++)
+		{
+			std::cout << "Delete Location: " << itl->first << std::endl;
+			delete itl->second;
+		}
+		std::cout << "Delete ServerConfig: " << its->listen << std::endl;
+		delete its;
 	}
 
 }
@@ -54,6 +100,13 @@ void	Config::_parseConfigFile(const std::string& filename)
 	}
 
 	std::string line;
+	ServerConfig* currentServer = new_serverConfig();
+	LocationConfig* currentLocation = new_locationConfig();
+	bool inTypesBlock = false;
+	bool inServerBlock = false;
+	bool inLocationBlock = false;
+	bool inErrorPagesBlock = false;
+
 	while (std::getline(file, line))
 	{
 		// Remove comments
@@ -69,122 +122,197 @@ void	Config::_parseConfigFile(const std::string& filename)
 		if (line.empty())
 			continue;
 
-		// Parse the line
-		if (line[line.length() - 1] == '{')
+		std::istringstream iss(line);
+		std::string key;
+		iss >> key;
+
+		if (key == "types")
 		{
-			std::string blockName = line.substr(0, line.size() - 1);
-			blockName.erase(blockName.find_last_not_of(" \t") + 1);
-			_parseBlock(file, blockName);
+			inTypesBlock = true;
 		}
-		else
+		else if (key == "server")
 		{
-			_parseKeyValuePair(line);
+			inServerBlock = true;
+			if (currentServer->server_name.empty() == false)
+			{
+				_servers.push_back(currentServer);
+				currentServer = new_serverConfig();
+			}
+		}
+		else if (key == "location")
+		{
+			inLocationBlock = true;
+			if (currentLocation->path.empty() == false)
+			{
+				currentServer->map_locations[currentLocation->path] = currentLocation;
+				currentLocation = new_locationConfig();
+			}
+			std::string locationPath;
+			iss >> locationPath;
+			currentLocation->path = locationPath;
+		}
+		else if (key == "error_pages")
+		{
+			inErrorPagesBlock = true;
+		}
+		else if (key == "}")
+		{
+			if (inErrorPagesBlock)
+			{
+				inErrorPagesBlock = false;
+			}
+			else if (inLocationBlock)
+			{
+				inLocationBlock = false;
+				if (currentLocation->allowed_methods.size() == 0)
+				{
+					currentLocation->allowed_methods.push_back("GET");
+					currentLocation->allowed_methods.push_back("POST");
+					currentLocation->allowed_methods.push_back("DELETE");
+				}
+				currentServer->map_locations[currentLocation->path] = currentLocation;
+				currentServer->map_locationObjs[currentLocation->path] = new Location(currentLocation);
+				currentServer->map_locationObjs[currentLocation->path]->setServer(currentServer);
+				currentLocation = new_locationConfig();
+			}
+			else if (inServerBlock)
+			{
+				inServerBlock = false;
+				_servers.push_back(currentServer);
+				currentServer = new_serverConfig();
+			}
+			else if (inTypesBlock)
+			{
+				inTypesBlock = false;
+			}
+		}
+		else if (inTypesBlock)
+		{
+			size_t delimPos = line.find_last_of("\t ");
+			std::string v = line.substr(0, delimPos);
+			std::string k = line.substr(delimPos + 1);
+			k.erase(k.length() - 1);
+			v.erase(0, v.find_first_not_of(" \t"));
+			v.erase(v.find_last_not_of(" \t") + 1);
+			_mimeTypeMap[k] = v;
+		}
+		else if (inErrorPagesBlock)
+		{
+			int code;
+			code = atoi(key.c_str());
+			std::string path;
+			iss >> path;
+			path.erase(path.length() - 1);
+			currentServer->error_pages[code] = path;
+		}
+		else if (inLocationBlock)
+		{
+			std::string val = "";
+			if (key == "root")
+			{
+				iss >> val;
+				val.erase(val.length() - 1);
+				currentLocation->root = val;
+			}
+			else if (key == "allowed_methods")
+			{
+				std::string delim = " ";
+				size_t pos = 0;
+				while ((pos = line.find(delim)) != std::string::npos)
+				{
+					val = line.substr(0, pos);
+					val.erase(0, val.find_first_not_of(" \t"));
+					val.erase(val.find_last_not_of(" \t") + 1);
+					currentLocation->allowed_methods.push_back(val);
+					line.erase(0, pos + delim.length());
+				}
+				val = line;
+				val.erase(val.length() - 1);
+				currentLocation->allowed_methods.push_back(val);
+			}
+			else if (key == "lsdir")
+			{
+				currentLocation->lsdir = true;
+			}
+			else if (key == "cgi_ext")
+			{
+				iss >> val;
+				val.erase(val.length() - 1);
+				currentLocation->cgi_ext = val;
+			}
+			else if (key == "redirection")
+			{
+				iss >> val;
+				val.erase(val.length() - 1);
+				currentLocation->redirection = val;
+			}
+			else if (key == "default_file")
+			{
+				iss >> val;
+				val.erase(val.length() - 1);
+				currentLocation->default_file = val;
+			}
+		}
+		else if (inServerBlock)
+		{
+			std::string value = "";
+			if (key == "server_name")
+			{
+				iss >> value;
+				value.erase(value.length() - 1);
+				currentServer->server_name = value;
+			}
+			else if (key == "listen")
+			{
+				iss >> value;
+				value.erase(value.length() - 1);
+				currentServer->listen = value;
+				std::string listen = currentServer->listen;
+				size_t delimPos = listen.find_first_of(":");
+				currentServer->port = atoi(listen.substr(delimPos + 1).c_str());
+				currentServer->host = listen.substr(0, delimPos);
+			}
+			else if (key == "max_body_size")
+			{
+				iss >> currentServer->max_body_size;
+			}
+			else if (key == "root")
+			{
+				iss >> value;
+				value.erase(value.length() - 1);
+				currentServer->root = value;
+			}
+			else if (key == "default_file")
+			{
+				iss >> value;
+				value.erase(value.length() - 1);
+				currentServer->default_file = value;
+			}
+			else if (key == "upload_dir")
+			{
+				iss >> value;
+				value.erase(value.length() - 1);
+				currentServer->upload_dir = value;
+			}
+			else if (key == "cgi_dir")
+			{
+				iss >> value;
+				value.erase(value.length() - 1);
+				currentServer->cgi_dir = value;
+			}
 		}
 	}
-}
 
-void	Config::_parseBlock(std::istream& stream, const std::string& blockName)
-{
-	std::string line;
-	while (std::getline(stream, line))
-	{
-		// Remove comments
-		size_t pos = line.find('#');
-		if (pos != std::string::npos)
-			line = line.substr(0, pos);
+	if (currentServer->server_name.empty() == false)
+		_servers.push_back(currentServer);
 
-		// Trim whitespace
-		line.erase(0, line.find_first_not_of(" \t"));
-		line.erase(line.find_last_not_of(" \t") + 1);
-
-		if (line.empty())
-			continue;
-
-		if (line[line.length() - 1] == '}')
-			return;
-
-		if (line[line.length() - 1] == '{')
-		{
-			std::string nestedBlockName = line.substr(0, line.size() - 1);
-			nestedBlockName.erase(nestedBlockName.find_last_not_of(" \t") + 1);
-			_parseBlock(stream, nestedBlockName);
-		}
-		else
-		{
-			if (blockName == "types")
-			{
-				size_t delimPos = line.find_last_of("\t ");
-				std::string value = line.substr(0, delimPos);
-				std::string key = line.substr(delimPos + 1);
-				key.erase(key.length() - 1);
-				value.erase(0, value.find_first_not_of(" \t"));
-				value.erase(value.find_last_not_of(" \t") + 1);
-				_mimeTypeMap[key] = value;
-			}
-			else if (blockName == "error_pages")
-			{
-				std::istringstream iss(line);
-				int code;
-				std::string path;
-				iss >> code >> path;
-				path.erase(path.length() - 1);
-				_errorPageMap[code] = path;
-			}
-			else if (blockName == "server")
-			{
-				_parseKeyValuePair(line);
-			}
-			else if (blockName.rfind("location", 0) == 0)
-			{
-				std::istringstream iss(blockName);
-				std::string block;
-				std::string path;
-				iss >> block >> path;
-
-				bool found = false;
-				Location* location = getLocation(path);
-				if (location == NULL)
-					location = new Location(path);
-				else
-					found = true;
-				setLocation(location, line);
-
-				if (found == false)
-					_locationMap[path] = location;
-			}
-		}
-	}
-}
-
-void	Config::_parseKeyValuePair(const std::string line)
-{
-	size_t delimPos = line.find_first_of("\t ");
-	std::string key = line.substr(0, delimPos);
-	std::string value = line.substr(delimPos + 1);
-
-	// Trim whitespace from key and value
-	key.erase(0, key.find_first_not_of(" \t"));
-	key.erase(key.find_last_not_of(" \t") + 1);
-	value.erase(0, value.find_first_not_of(" \t"));
-	value.erase(value.find_last_not_of(" \t") + 1);
-	value.erase(value.length() - 1);
-
-	_configMap[key] = value;
-}
-
-void	Config::_setHostPort()
-{
-	std::string listen = get("listen");
-	size_t delimPos = listen.find_first_of(":");
-	// _port = atoi(listen.substr(delimPos + 1).c_str());
-	_port = atoi(listen.substr(delimPos + 1).c_str());
-	_host = listen.substr(0, delimPos);
+	delete currentServer;
+	delete currentLocation;
 }
 
 void	Config::load(const std::string& filename)
 {
 	_parseConfigFile(filename);
-	_setHostPort();
 }
 
 void	Config::setLocation(Location* location, std::string line)
@@ -236,24 +364,29 @@ std::map<std::string, std::string>	Config::getConfigMap() const
 	return (_configMap);
 }
 
-std::map<std::string, Location*>	Config::getLocationMap() const
-{
-	return (_locationMap);
-}
+// std::map<std::string, Location*>	Config::getLocationMap() const
+// {
+// 	return (_locationMap);
+// }
 
-std::map<std::string, std::string>	Config::getServerSettingMap() const
-{
-	return (_serverSettingMap);
-}
+// std::map<std::string, std::string>	Config::getServerSettingMap() const
+// {
+// 	return (_serverSettingMap);
+// }
 
-std::map<int, std::string>	Config::getErrorPageMap() const
-{
-	return (_errorPageMap);
-}
+// std::map<int, std::string>	Config::getErrorPageMap() const
+// {
+// 	return (_errorPageMap);
+// }
 
 std::map<std::string, std::string>	Config::getMimeTypeMap() const
 {
 	return (_mimeTypeMap);
+}
+
+std::vector<ServerConfig*>	Config::getServers() const
+{
+	return (_servers);
 }
 
 std::string	Config::get(const std::string key) const
@@ -283,30 +416,30 @@ int	Config::getInt(const std::string key) const
 	return (atoi(value.c_str()));
 }
 
-std::string	Config::getServerHost() const
-{
-	return (_host);
-}
+// std::string	Config::getServerHost() const
+// {
+// 	return (_host);
+// }
 
-int	Config::getPort() const
-{
-	return (_port);
-}
+// int	Config::getPort() const
+// {
+// 	return (_port);
+// }
 
-std::string	Config::getErrorPage(const int code) const
-{
-	std::map<int, std::string>::const_iterator it;
-	it = _errorPageMap.find(code);
-	if (it == _errorPageMap.end())
-		return ("");
-	return (it->second);
-}
+// std::string	Config::getErrorPage(const int code) const
+// {
+// 	std::map<int, std::string>::const_iterator it;
+// 	it = _errorPageMap.find(code);
+// 	if (it == _errorPageMap.end())
+// 		return ("");
+// 	return (it->second);
+// }
 
-Location*	Config::getLocation(const std::string key) const
-{
-	std::map<std::string, Location*>::const_iterator it;
-	it = _locationMap.find(key);
-	if (it == _locationMap.end())
-		return (NULL);
-	return (it->second);
-}
+// Location*	Config::getLocation(const std::string key) const
+// {
+// 	std::map<std::string, Location*>::const_iterator it;
+// 	it = _locationMap.find(key);
+// 	if (it == _locationMap.end())
+// 		return (NULL);
+// 	return (it->second);
+// }
